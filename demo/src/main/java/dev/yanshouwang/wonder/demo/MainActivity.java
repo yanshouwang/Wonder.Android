a9a1a1743cb1815e26a77d602754c5091ad7b43c
闫守旺 <yanshouwang@outlook.com>
2020-01-29 23:07:58 +0800

    解决部分文件未添加到 VCS 的问题

---
+++ demo/src/main/java/dev/yanshouwang/wonder/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.yanshouwang.wonder.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
